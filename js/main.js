a24f3831fac0980949c2063a5c9fcd74fe61d52d
CaioDevPR <programadorpr@outlook.com>
2023-03-05 12:52:40 -0300

    Configuracao de API email

--- js/main.js
+++ js/main.js
@@ -7,3 +7,13 @@ function openMenu(){
 function closeMenu(){
     ul.classList.remove('open');
 }
+
+function sendMail(){
+    var params = {
+        from_name : document.getElementById("fullName").value,
+        email_id  : document.getElementById("email_id").value,
+        message : document.getElementById("message").value,
+    }
+
+    emailjs.send("servie")
+}
